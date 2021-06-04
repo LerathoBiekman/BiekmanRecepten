6ac05874aa30ddc4a0e0ccd02ee7b93c234c5812
Leratho Biekman <biekman.l@hotmail.com>
2021-06-04 19:31:42 +0200

    The functionality of the recipe add page is working

--- biekmanRecepten/models.py
+++ biekmanRecepten/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    dish = models.CharField(max_length=20)
+    season = models.CharField(max_length=20)
+    prep_time = models.IntegerField()
+    people = models.IntegerField()
+    description = models.TextField(default=None)
+
+    class Meta:
+        db_table = 'Recipe'
+
+    def __str__(self):
+        return "%s / %s / %s / %s / %s" % (self.title, self.dish, self.season, self.prep_time, self.people)
--- biekmanRecepten/views.py
+++ biekmanRecepten/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Recipe
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def add_recipe(request):
+    if request.method == 'POST':
+        if request.POST.get("submit"):
+            recipe = Recipe()
+            recipe.title = request.POST['title']
+            recipe.dish = request.POST['dish']
+            recipe.season = request.POST['season']
+            recipe.prep_time = request.POST['prep_time']
+            recipe.description = request.POST['description']
+            recipe.people = request.POST['people']
+            recipe.save()
+    dish = ["Vlees", "Vis", "Vegetarisch", "Seafood", "Dessert", "Apperatief", "Anders"]
+    seasons = ["Winter", "Lente", "Zomer", "Herfst", "Meerdere seizoenen"]
+    return render(request, 'add_recipe.html', {
+        'dish': dish,
+        'seasons': seasons,
+    })
